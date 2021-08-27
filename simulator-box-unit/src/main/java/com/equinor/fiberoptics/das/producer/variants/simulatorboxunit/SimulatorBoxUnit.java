748282a33bc3630aa3f63cfa4c2c76e0c9cf325b
Inge Knudsen <iknu@equinor.com>
2021-08-27 09:16:03 +0200

    Break up into modules

---
+++ simulator-box-unit/src/main/java/com/equinor/fiberoptics/das/producer/variants/simulatorboxunit/SimulatorBoxUnit.java
@@ -0,0 +1,42 @@
+/*-
+ * ========================LICENSE_START=================================
+ * fiberoptics-das-producer
+ * %%
+ * Copyright (C) 2020 Equinor ASA
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+package com.equinor.fiberoptics.das.producer.variants.simulatorboxunit;
+
+import com.equinor.fiberoptics.das.producer.variants.GenericDasProducer;
+import org.springframework.stereotype.Component;
+
+/**
+ * This is an example DAS  box unit implementation.
+ * It's role is to convert the raw DAS data into a format that can be accepted into the Kafka server environment.
+ * Serving the amplitude data
+ *
+ * @author Espen Tjonneland, espen@tjonneland.no
+ */
+@Component("SimulatorBoxUnit")
+public class SimulatorBoxUnit implements GenericDasProducer {
+
+  public boolean isDone() {
+    return true;
+  }
+
+  public void startDataStreaming() {
+
+  }
+}
