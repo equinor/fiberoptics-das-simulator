95b6ed8d0431d05a094b213f41c5d28698890b38
espentj <estjo@equinor.com>
2021-09-14 15:35:33 +0200

    Fixed examples scenarios.

--- dependson-services/sql/V1.2.1__insert_test_data.sql
+++ dependson-services/sql/V1.2.1__insert_test_data.sql
@@ -4,13 +4,28 @@ SELECT '25be8bc6-cd35-4c9f-9f8d-cabe43326163', 'Simulator box fiber path', 0, '{
 
 -- vendor_code
 INSERT INTO vendor(id, name, vendor_code, api_key)
-SELECT '3a1e813f-a272-498c-8850-6188587369cd', 'Simulator vendor', 'Simulator', 'abcdefghijklmno12345678901234567'
+SELECT '3a1e813f-a272-498c-8850-6188587369cd', 'Simulator vendor', 'Simulator', '1aa111a11aa11a0a1a1aa1111a1a1a1a'
     WHERE NOT EXISTS(SELECT id from vendor WHERE id = '3a1e813f-a272-498c-8850-6188587369cd');
 
 -- instrument_box
 INSERT INTO instrument_box(id, description, instrument_box_info, vendor_id)
 SELECT '00528e45-06d0-4110-bba4-e904aaa02657', 'Simulator 1', '{}', '3a1e813f-a272-498c-8850-6188587369cd'
     WHERE NOT EXISTS(SELECT id from instrument_box WHERE id = '00528e45-06d0-4110-bba4-e904aaa02657');
+
+-- instrument_box 2
+INSERT INTO instrument_box(id, description, instrument_box_info, vendor_id)
+SELECT '1deb5d57-2fb9-418a-990b-4cf7252a0450', 'Simulator 2', '{}', '3a1e813f-a272-498c-8850-6188587369cd'
+WHERE NOT EXISTS(SELECT id from instrument_box WHERE id = '1deb5d57-2fb9-418a-990b-4cf7252a0450');
+
+-- instrument_box 3
+INSERT INTO instrument_box(id, description, instrument_box_info, vendor_id)
+SELECT '1deb5d57-2fb9-418a-990b-4cf7252a0451', 'Simulator 3', '{}', '3a1e813f-a272-498c-8850-6188587369cd'
+WHERE NOT EXISTS(SELECT id from instrument_box WHERE id = '1deb5d57-2fb9-418a-990b-4cf7252a0451');
+
+-- instrument_box 4
+INSERT INTO instrument_box(id, description, instrument_box_info, vendor_id)
+SELECT '1deb5d57-2fb9-418a-990b-4cf7252a0452', 'Simulator 4', '{}', '3a1e813f-a272-498c-8850-6188587369cd'
+WHERE NOT EXISTS(SELECT id from instrument_box WHERE id = '1deb5d57-2fb9-418a-990b-4cf7252a0452');
 
 -- A test profile
 INSERT INTO profile(id, description,
