a666924a7a9a45f766ce176cd2c5c424a92d031f
espentj <estjo@equinor.com>
2021-09-14 12:00:46 +0200

    Restructure POM.
    [maven-release-plugin] rollback the release of fiberoptics-das-simulator-2.0.0
    
    [maven-release-plugin] prepare release fiberoptics-das-simulator-2.0.0
    
    Restructure POM.
    
    Restructure POM.

--- simulator-box-unit/src/test/java/com/equinor/fiberoptics/das/producer/variants/simulatorboxunit/SimulatorBoxUnitTest.java
+++ simulator-box-unit/src/test/java/com/equinor/fiberoptics/das/producer/variants/simulatorboxunit/SimulatorBoxUnitTest.java
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * simulator-box-unit
+ * %%
+ * Copyright (C) 2020 - 2021 Equinor ASA
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 package com.equinor.fiberoptics.das.producer.variants.simulatorboxunit;
 
 import com.equinor.fiberoptics.das.producer.variants.PartitionKeyValueEntry;
@@ -53,12 +72,8 @@ public class SimulatorBoxUnitTest {
     CountDownLatch latch = new CountDownLatch(1);
     simulatorBoxUnit.produce()
       .subscribe(logOutput,
-      (ex) -> {
-        logger.info("Error emitted: " + ex.getMessage());
-      },
-      () -> {
-        latch.countDown();
-      });
+      (ex) -> logger.info("Error emitted: " + ex.getMessage()),
+        latch::countDown);
 
     Helpers.wait(latch);
 
