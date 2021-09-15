37225b95bd5c138b1c8bbf128612671dcbf1588f
espentj <estjo@equinor.com>
2021-09-15 12:59:04 +0200

    Merge in from main

--- static-data-unit/src/main/java/com/equinor/fiberoptics/das/producer/variants/staticdataunit/StaticDataUnitConfiguration.java
+++ static-data-unit/src/main/java/com/equinor/fiberoptics/das/producer/variants/staticdataunit/StaticDataUnitConfiguration.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +22,6 @@ package com.equinor.fiberoptics.das.producer.variants.staticdataunit;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import javax.annotation.PostConstruct;
 
 /**
  * Configuration class for the DAS box unit
